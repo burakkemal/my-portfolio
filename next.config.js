836c55c66aeeb0dbdbcbd1ec2321dd9a131bd42a
Burak Kemal Koyuncu <burakkemalkoyuncu@gmail.com>
2025-02-21 23:58:40 +0300

    last commit

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
-/**
- * @type {import('next').NextConfig}
- */
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  images: {
+    domains: ['your-domain.com'], // Eğer harici bir URL kullanıyorsanız
+    unoptimized: true,
+  },
 }
- 
-module.exports = nextConfig
+
+module.exports = nextConfig 
